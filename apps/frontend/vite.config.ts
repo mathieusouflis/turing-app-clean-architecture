792c5ef38d06ec434a6d4701641a51cdaeb9d52b
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-12 16:54:12 +0100

    feat: Setup tanstack router and Shadcn

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,9 +3,24 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      tanstackRouter({
+        // target: "react",
+        // autoCodeSplitting: true,
+        routesDirectory: "./src/app",
+      }),
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     server: {
       host: "0.0.0.0",
       port: 5173,
