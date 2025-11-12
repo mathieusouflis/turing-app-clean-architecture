f563e078cd986146df7625221dd9e049977c9772
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-12 16:52:42 +0100

    feat: Auto code generation tanstack router

--- apps/frontend/src/app/providers.tsx
+++ apps/frontend/src/app/providers.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/providers')({
+export const Route = createFileRoute("/providers")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/providers"!</div>
+  return <div>Hello "/providers"!</div>;
 }
