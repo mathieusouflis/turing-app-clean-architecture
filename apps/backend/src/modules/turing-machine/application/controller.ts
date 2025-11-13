dc8e61b0a035c04555736f0d4bfb46893208f0eb
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-13 21:40:45 +0100

    feat: Add delete support for Turing machines
    Inject DeleteUseCase, implement delete handler to execute the use case
    and return 204. Wire DeleteUseCase into routes and convert controller
    methods/registerRoutes to arrow functions to preserve this binding.

--- apps/backend/src/modules/turing-machine/application/controller.ts
+++ apps/backend/src/modules/turing-machine/application/controller.ts
@@ -1,22 +1,26 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { CreateUseCase } from "./use-cases/create";
 import { NewTuringMachineRecordSchema } from "../shemas/turing-machine";
+import { DeleteUseCase } from "./use-cases/delete";
 
 export class TuringMachineController {
-  constructor(private createUseCase: CreateUseCase) {}
+  constructor(
+    private createUseCase: CreateUseCase,
+    private deleteUseCase: DeleteUseCase,
+  ) {}
 
-  public registerRoutes(fastify: FastifyInstance, ops: any, done: any) {
-    fastify.post("/", this.create.bind(this));
-    fastify.get("/:id", this.getById.bind(this));
-    fastify.get("/", this.list.bind(this));
-    fastify.put("/:id/step", this.step.bind(this));
-    fastify.put("/:id/run", this.run.bind(this));
-    fastify.delete("/:id", this.delete.bind(this));
+  public registerRoutes = (fastify: FastifyInstance, ops: any, done: any) => {
+    fastify.post("/", this.create);
+    fastify.get("/:id", this.getById);
+    fastify.get("/", this.list);
+    fastify.put("/:id/step", this.step);
+    fastify.put("/:id/run", this.run);
+    fastify.delete("/:id", this.delete);
 
     done();
-  }
+  };
 
-  private async create(request: FastifyRequest, reply: FastifyReply) {
+  private create = async (request: FastifyRequest, reply: FastifyReply) => {
     const data = request.body;
     const dataParsed = NewTuringMachineRecordSchema.safeParse(data);
 
@@ -28,15 +32,21 @@ export class TuringMachineController {
     const machine = await this.createUseCase.execute(dataParsed.data);
 
     reply.status(201).send(machine);
-  }
+  };
 
-  private async getById(request: FastifyRequest, reply: FastifyReply) {}
+  private getById = async (request: FastifyRequest, reply: FastifyReply) => {};
 
-  private async list(request: FastifyRequest, reply: FastifyReply) {}
+  private list = async (request: FastifyRequest, reply: FastifyReply) => {};
 
-  private async step(request: FastifyRequest, reply: FastifyReply) {}
+  private step = async (request: FastifyRequest, reply: FastifyReply) => {};
 
-  private async run(request: FastifyRequest, reply: FastifyReply) {}
+  private run = async (request: FastifyRequest, reply: FastifyReply) => {};
 
-  private async delete(request: FastifyRequest, reply: FastifyReply) {}
+  private delete = async (request: FastifyRequest, reply: FastifyReply) => {
+    const { id } = request.params as { id: string };
+
+    await this.deleteUseCase.execute(id);
+
+    reply.status(204).send();
+  };
 }
