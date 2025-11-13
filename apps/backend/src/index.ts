79b10e07cee3713035e9542d25c9c4eb76c3a945
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-13 16:53:45 +0100

    fix(dotenv-config-backend): Fix backend dotenv path

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,7 +2,7 @@ import fastify from "fastify";
 import dotenv from "dotenv";
 import path from "path";
 
-const envPath = path.resolve(__dirname, "../../../.env.dev");
+const envPath = path.resolve(__dirname, "../../../../.env.dev");
 dotenv.config({ path: envPath });
 
 const server = fastify();
