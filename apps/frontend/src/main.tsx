2915d6499b35f198a83bd52114e2840bbc66ec4f
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-14 14:16:47 +0100

    feat: Add React Query client and provider

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,24 @@
 import { createRoot } from "react-dom/client";
 import "./assets/index.css";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { routeTree } from "./routeTree.gen";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 const router = createRouter({ routeTree });
 
 createRoot(document.getElementById("root")!).render(
   <>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </>,
 );
